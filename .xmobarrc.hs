de9916bb3c97a5365002139f9220d94a917cae44
bernie <bernie@tutanota.com>
2016-07-03 15:21:10 +0200

    xmobar

--- .xmobarrc.hs
+++ .xmobarrc.hs
@@ -7,7 +7,7 @@ Config { font = "xft:Inconsolata:pixelsize=16:antialias=true"
        , persistent = True
        , hideOnStart = False
        , allDesktops = True
-       , overrideRedirect = True
+       , overrideRedirect = False
        , commands = [ Run Cpu ["-L","0","-H","50","--normal","green"] 10
                     , Run Memory ["-L","0","-H","50","--normal","green", "-t","MEM: <usedratio>%"] 5
                     , Run Network "eth0" ["-L","0","-H","60","--normal","green","--high","red"] 5
@@ -19,14 +19,3 @@ Config { font = "xft:Inconsolata:pixelsize=16:antialias=true"
        , template = " %StdinReader% }{ %cpu%   %memory%  %eth0%   %date% "
        }
 
-
--- font = "xft:Terminus:size=8:antialias=true"
---       , commands = [ Run Cpu ["-L","0","-H","50","--normal","green"] 10
---                    , Run Memory ["-L","0","-H","50","--normal","green", "-t","MEM: <usedratio>%"] 5
---                    , Run MPD ["-t", "<state>: <artist> - <title> <lapsed>/<length>"] 10
---                    , Run Network "eth0" ["-L","0","-H","60","--normal","green","--high","red"] 5
---                    , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
---                    , Run StdinReader
---                    ]
-
---       , template = " %StdinReader% }{ %mpd%   %cpu%   %memory%  %eth0%   %date% "
