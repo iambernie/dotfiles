d1dcc15b1f605a16536c84d93ad92dbf8463f66c
bernie <bernie@tutanota.com>
2016-10-28 08:01:53 +0200

    new xmobar_bottom.hs

---
+++ .xmobarrc_bottom.hs
@@ -0,0 +1,22 @@
+Config { font = "xft:Inconsolata:pixelsize=14:antialias=true"
+       , bgColor = "black"
+       , fgColor = "#29acff"
+       , position = Bottom
+       , lowerOnStart = False
+       , pickBroadest = False
+       , persistent = True
+       , hideOnStart = False
+       , allDesktops = True
+       , overrideRedirect = False
+       , commands = [ Run MarqueePipeReader "No IRQ activity:/home/bernie/pipes/mpipereader" (40, 5, "+") "mpipe"
+                    , Run PipeReader "pipereader:/home/bernie/pipes/pipereader" "pipe"
+                    , Run Com "/home/bernie/wireless.sh" [] "wifi" 20
+                    , Run Com "/home/bernie/volume.py" [] "volume" 10
+                    , Run Com "/home/bernie/battery.sh" [] "bat" 20
+
+                    ]
+       , sepChar = "%"
+       , alignSep = "}{"
+       , template = " %pipe% }{ %bat%  %volume%  %wifi%   IRC: %mpipe% "
+       }
+
