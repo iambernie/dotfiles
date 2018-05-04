efa2c0e8921baa64cec65d73cb006c6ce37df6a8
bernie <bernie@tutanota.com>
2018-05-04 07:51:26 +0200

    use python version

--- .xmobarrc_bottom.hs
+++ .xmobarrc_bottom.hs
@@ -10,7 +10,7 @@ Config { font = "xft:Inconsolata:pixelsize=14:antialias=true"
        , overrideRedirect = False
        , commands = [ Run MarqueePipeReader "No IRQ activity:/home/bernie/pipes/mpipereader" (40, 5, "+") "mpipe"
                     , Run PipeReader "pipereader:/home/bernie/pipes/pipereader" "pipe"
-                    , Run Com "/home/bernie/menuscripts/wireless.sh" [] "wifi" 20
+                    , Run Com "/home/bernie/menuscripts/wifisignal.py" [] "wifi" 20
                     , Run Com "/home/bernie/menuscripts/volume.py" [] "volume" 10
                     , Run Com "/home/bernie/menuscripts/battery.sh" [] "bat" 20
 
